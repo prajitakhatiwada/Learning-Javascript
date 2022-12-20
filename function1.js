6b4f667c4708bfe080624050ff12937a342b1031
prajitakhatiwada <khatiwadaprajita104@gmail.com>
2022-12-20 23:27:22 +0545

    functions

---
+++ function1.js
@@ -0,0 +1,11 @@
+function addNumber(num1,num2,num3,num4){
+    return num1+num2+num3+num4
+}
+let sum=addNumber(7,8,92,56789)
+console.log(sum)
+
+function subNumber(num1,num2){
+    return num1-num2
+}
+let minus=subNumber(678,88974)
+console.log(minus)

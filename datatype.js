243edc4d2562ca2815487d7b294975affe1e0c72
prajitakhatiwada <khatiwadaprajita104@gmail.com>
2022-12-20 23:09:32 +0545

    learned integer and string

---
+++ datatype.js
@@ -0,0 +1,10 @@
+let x=10 //yo integer ho
+let y=5.5 //yo float ho
+let a="10" //yo string ho
+let b="5" //yo string ho
+let firstname="Prajita"
+let lastname="Khatiwada"
+console.log(x+y)
+console.log(a+b)
+console.log(firstname+lastname)
+console.log(x+a)
